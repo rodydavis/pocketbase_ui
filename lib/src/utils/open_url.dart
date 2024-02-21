0fff72a814815f787050d0a06266235c1391b586
Rody Davis <rody.davis.jr@gmail.com>
2024-02-20 22:46:33 -0800

    fix open url

--- lib/src/utils/open_url.dart
+++ lib/src/utils/open_url.dart
@@ -3,7 +3,7 @@ import 'package:url_launcher/url_launcher.dart' hide launch;
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 
 Future<void> openUrl(Uri url) async {
-  if (platform.isMobile) {
+  if (!platform.isWeb && platform.isMobile) {
     await launch(url.toString());
   } else {
     await launchUrl(url);
